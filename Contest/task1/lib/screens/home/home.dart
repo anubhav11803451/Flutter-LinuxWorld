e3d856fad2895cd3130d10f51af4c4c3861aa4f9
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-08-01 22:11:14 +0530

    Finished desiging splash screen

---
+++ Contest/task1/lib/screens/home/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+      backgroundColor: Colors.black,
+    );
+  }
+}
---
+++ Contest/task1/lib/screens/splash/splash.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:task1/screens/home/home.dart';
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        height: size.height,
+        width: size.width,
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Center(
+              heightFactor: 4,
+              child: CircleAvatar(
+                radius: 35.0,
+                backgroundColor: Colors.black,
+                child: Image.asset('assets/icons/wave.png'),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Hey There!",
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  "We promise, you wil have hassel free\nstreaming of music and video",
+                  style: GoogleFonts.lato(
+                    color: Colors.white54,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: InkWell(
+                child: CircleAvatar(
+                  radius: 32.0,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  child: Icon(Icons.arrow_forward_ios),
+                ),
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
