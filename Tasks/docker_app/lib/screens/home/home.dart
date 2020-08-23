91cfa360e9ddc5b19b557dec9f21dd3adf5d8184
anubhav11803451 <anubhav.11803451@lpu.in>
2020-08-23 19:52:53 +0530

    Update home.dart

--- Tasks/docker_app/lib/screens/home/home.dart
+++ Tasks/docker_app/lib/screens/home/home.dart
@@ -30,76 +30,101 @@ class Homebody extends StatelessWidget {
               color: Colors.white24,
             ),
           ),
-          // SafeArea(
-          //   child: Padding(
-          //     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: <Widget>[
-          //         Align(
-          //           alignment: Alignment.topRight,
-          //           child: Container(
-          //             alignment: Alignment.center,
-          //             height: 52,
-          //             width: 52,
-          //             decoration: BoxDecoration(
-          //               color: Colors.white38,
-          //               shape: BoxShape.circle,
-          //             ),
-          //             child: IconButton(
-          //               icon: Icon(
-          //                 Icons.menu,
-          //                 color: Colors.white,
-          //               ),
-          //               onPressed: () {},
-          //             ),
-          //           ),
-          //         ),
-          //         Text(
-          //           'Docker it',
-          //           style: GoogleFonts.lato(
-          //             color: Colors.white,
-          //             fontWeight: FontWeight.w900,
-          //             fontSize: 20,
-          //           ),
-          //         ),
-          //         // Container(
-          //         //   margin: EdgeInsets.symmetric(vertical: 45),
-          //         //   padding:
-          //         //       EdgeInsets.symmetric(horizontal: 30, vertical: 2.1),
-          //         //   decoration: BoxDecoration(
-          //         //       color: Colors.white,
-          //         //       borderRadius: BorderRadius.circular(29.5)),
-          //         //   child: TextField(
-          //         //     decoration: InputDecoration(
-          //         //       hintText: "Search",
-          //         //       icon: Icon(
-          //         //         Icons.search,
-          //         //         color: Colors.black,
-          //         //       ),
-          //         //       border: InputBorder.none,
-          //         //     ),
-          //         //   ),
-          //         // ),
-          //         Expanded(
-          //           child: GridView.count(
-          //             crossAxisCount: 2,
-          //             childAspectRatio: .85,
-          //             crossAxisSpacing: 20,
-          //             mainAxisSpacing: 20,
-          //             children: <Widget>[
-          //               Container(
-          //                 height: 80,
-          //                 width: 80,
-          //                 color: Colors.white,
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Colors.white38,
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Docker it',
+                    style: GoogleFonts.lato(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 45),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30, vertical: 2.1),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(29.5)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        Container(
+                          height: 80,
+                          width: 80,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
